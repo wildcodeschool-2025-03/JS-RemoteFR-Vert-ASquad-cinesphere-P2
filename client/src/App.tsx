65c10fa10fc0bf25fa831956dd7aac8cdee446c9
Alexandre R. <sadnessnc@gmail.com>
2025-04-18 11:46:04 +0200

    Feat: Update main title in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <header>
-        <h1 className="logo">JS Monorepo</h1>
+        <h1 className="logo">CineSphere</h1>
       </header>
 
       <nav className="navbar">
