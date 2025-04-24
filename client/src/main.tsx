8b4de7f9d507b1b42c33a3bee416daaf8a62b29c
MKNG18 <naili.maeva@orange.fr>
2025-04-24 10:34:39 +0200

    fix: resolved issue with mobile display

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -39,7 +39,7 @@ if (rootElement == null) {
 createRoot(rootElement).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>
+  </StrictMode>,
 );
 
 /**
---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import MainTopCardList from "../components/MainTopCardList";
+
+function HomePage() {
+  return (
+    <div>
+      <MainTopCardList />
+    </div>
+  );
+}
+
+export default HomePage;
