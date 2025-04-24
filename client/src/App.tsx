6634bb0b7708f89885bf23c4e351ce20df31ffc9
MKNG18 <naili.maeva@orange.fr>
2025-04-24 15:41:15 +0200

    fix: added .env file and outlets

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,10 @@
 import "./styles/MainTopCardList.css";
-import HomePage from "./pages/HomePage";
+import { Outlet } from "react-router";
 
 function App() {
   return (
     <>
-      <HomePage />
+      <Outlet />
     </>
   );
 }
