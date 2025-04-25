f745b5f77970dc3be418f0ecc3e1e7a45574eb25
Alexandre R. <sadnessnc@gmail.com>
2025-04-25 15:29:56 +0200

    fix: resolve bugs*

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,26 +1,17 @@
-import Burger from "../components/Burger";
-import Footer from "../components/Footer";
 import HeaderButton from "../components/HeaderButton";
+import HeaderBanner from "../components/HeaderButton";
 import MainBottom from "../components/MainBottom";
 import MainTopCardList from "../components/MainTopCardList";
-import Menu from "../components/Menu";
-import NavBar from "../components/NavBar";
 
 function HomePage() {
   return (
     <div>
       <main>
-        <NavBar />
-        <Burger />
-        <Menu />
         <HeaderButton />
         <HeaderBanner />
         <MainTopCardList />
         <MainBottom />
       </main>
-      <footer>
-        <Footer />
-      </footer>
     </div>
   );
 }
