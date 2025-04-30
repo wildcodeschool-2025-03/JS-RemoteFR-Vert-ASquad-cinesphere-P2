cb089cee1564c9f07bea4ebc56d440996561fc9b
Alexandre R. <sadnessnc@gmail.com>
2025-04-30 17:38:48 +0200

    chore: fix SelectionMovieList error

--- client/src/components/selection/SelectionMovieList.tsx
+++ client/src/components/selection/SelectionMovieList.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import type { FC } from "react";
 import SelectionMovieCard from "./SelectionMovieCard";
 import "../../assets/styles/SelectionMovieList.css";
