a85b043e19945de380970a44810afc4bc420620b
Alexandre R. <sadnessnc@gmail.com>
2025-05-14 16:03:56 +0200

    fix: fix headerbanner & button responsive

--- client/src/components/HeaderBanner.tsx
+++ client/src/components/HeaderBanner.tsx
@@ -3,17 +3,23 @@ import "../assets/styles/HeaderBanner.css";
 
 type HeaderBannerProps = {
   imgSrc?: string;
-  title?: string;
+  titleSrc?: string;
 };
 
 const HeaderBanner: FC<HeaderBannerProps> = ({
   imgSrc = "https://i.imgur.com/Eh9QyEb.png",
-  title = "LA RÉPARATION",
+  titleSrc = "https://cdn.bemaup.com/4_zc4e2bf4ddbd23e0c9d610c11_f105493bd58856de2_d20250514_m132403_c003_v0312005_t0023_u01747229043703",
 }) => {
   return (
     <>
-      <img className="banner-img" src={imgSrc} alt="Banner Film" />
-      <h1 className="banner-title">{title}</h1>
+      <div className="banner-img">
+        <img
+          className="banner-background"
+          src={imgSrc}
+          alt="Banner Film Background"
+        />
+        <img className="banner-title" src={titleSrc} alt="Banner Title" />
+      </div>
     </>
   );
 };
